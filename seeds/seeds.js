aa83f075572bc95ffbd894c36515814a17069cf9
Nicholas Leask <leask.nicholas@gmail.com>
2018-06-07 19:17:36 -0400

    Added Procfile

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -11,7 +11,6 @@ exports.seed = function(knex, Promise) {
         size: 17026,
         poster_url: 'https://www.suny.edu/media/suny/content-assets/images/campus-profiles/pics/StonyBrook-walkway-flag.jpg',
         keywords: 'chemistry, science, STEM, difficult, big, SUNY, State University of New York, men, commuter school, pre med, pre-med, State University of NY, sororities, sorority, frat, biology, chinese, china, korean, korea, japan, japanese, india, indian, bengali, bengal, asia, asian, greek life, clubs, LIRR, train, baseball, football, basketball, soccer, lacrosse',
-        negativeKeywords: ,
         gpa: '3.8',
         cost: 26239,
       },{
@@ -21,7 +20,6 @@ exports.seed = function(knex, Promise) {
         size: 67556,
         poster_url: 'https://stjenglish.com/wp-content/uploads/RUTGERS-678x381.png',
         keywords: 'expensive, public, crowded, waste, scam, bad teaching, lies, party, fraternities, sororities, math, mathematics, 5 year masters, business, huge, expansive, food, good food, greek life, clubs, baseball, football, basketball, soccer, lacrosse, softball, wrestling',
-        negativeKeywords: ,
         gpa: '3.66',
         cost: 48132,
       },{
@@ -31,7 +29,6 @@ exports.seed = function(knex, Promise) {
         size: 32429,
         poster_url: 'https://gs.columbia.edu/files/gs/columbia-university-veteran-center-logo-coin-web.png',
         keywords: 'bernard, library, architecture, statue, NYC, UWS, ivy-league, ivy, ivy league, small, business, engineering, science,  private, leadership, activists, activism, intern, work, food, good food',
-        negativeKeywords: ,
         gpa: '4.0',
         cost: 74173,
       },{
